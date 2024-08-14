a5859fbd02787f16726e07859d457ca55ce99957
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-08-15 01:00:19 +0200

    Now supporting root AND Phoenix directory

--- LRCleaner.bat
+++ LRCleaner.bat
@@ -1,5 +1,11 @@
 @echo off
 
+IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleaningHogwarts
+IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleaningHogwarts2
+ECHO This game is not supported for now.
+GOTO :end
+
+:cleaningHogwarts
 echo Deleting mod files...
 RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
 DEL "Phoenix\Binaries\Win64\cudart64_110.dll"
@@ -16,6 +22,25 @@ SET "dst=%LOCALAPPDATA%%sub%"
 DEL "%dst%GameUserSettings.ini"
 COPY "%dst%GameUserSettings_ori.ini" "%dst%GameUserSettings.ini"
 DEL "%dst%GameUserSettings_ori.ini"
+GOTO end
 
-echo Hogwarts Legacy now cleaned. Please report any issues on my GitHub: Yelodress
+
+:cleaningHogwarts2
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log" "RealConfig.bat" 
+
+echo Restoring graphic settings...
+SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%GameUserSettings.ini"
+REN "%dst%GameUserSettings_ori.ini" "%dst%GameUserSettings.ini" 
+GOTO end
+
+:end
+echo All done ! Please report any issues on my GitHub: Yelodress
+pause
+
+:abort
+ECHO Aborting LRCleaner.
 pause

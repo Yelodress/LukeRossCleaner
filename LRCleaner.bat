dfe09c0d04b38b94423333c4a7a1e4d17bec2917
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-08-15 00:17:20 +0200

    now deleting VR graphic settings

--- LRCleaner.bat
+++ LRCleaner.bat
@@ -1,12 +1,21 @@
 @echo off
 
-rmdir /s /q "Phoenix\Binaries\Win64\RealRepo"
-del "Phoenix\Binaries\Win64\cudart64_110.dll"
-del "Phoenix\Binaries\Win64\dxgi.dll"
-del "Phoenix\Binaries\Win64\openvr_api.dll"
-del "Phoenix\Binaries\Win64\RealConfig.bat"
-del "Phoenix\Binaries\Win64\RealVR.ini"
-del "Phoenix\Binaries\Win64\RealVR64.log"
+echo Deleting mod files...
+RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
+DEL "Phoenix\Binaries\Win64\cudart64_110.dll"
+DEL "Phoenix\Binaries\Win64\dxgi.dll"
+DEL "Phoenix\Binaries\Win64\openvr_api.dll"
+DEL "Phoenix\Binaries\Win64\RealConfig.bat"
+DEL "Phoenix\Binaries\Win64\RealVR.ini"
+DEL "Phoenix\Binaries\Win64\RealVR64.log"
+DEL "Phoenix\Binaries\Win64\RealConfig.bat"
+
+echo Restoring graphic settings...
+SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%GameUserSettings.ini"
+COPY "%dst%GameUserSettings_ori.ini" "%dst%GameUserSettings.ini"
+DEL "%dst%GameUserSettings_ori.ini"
 
 echo Hogwarts Legacy now cleaned. Please report any issues on my GitHub: Yelodress
 pause

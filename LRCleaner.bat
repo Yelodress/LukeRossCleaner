d8d2c5b9e5805ee816b6716ddc83d6b44cd8b2e6
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-08-17 02:16:20 +0200

    Added Cyberpunk 2077 support

--- LRCleaner.bat
+++ LRCleaner.bat
@@ -1,11 +1,12 @@
 @echo off
 
-IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleaningHogwarts
-IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleaningHogwarts2
+IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts
+IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts2
+IF EXIST Cyberpunk2077.exe GOTO cleanupCP2077
 ECHO This game is not supported for now.
 GOTO :end
 
-:cleaningHogwarts
+:cleanupHogwarts
 echo Deleting mod files...
 RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
 DEL "Phoenix\Binaries\Win64\cudart64_110.dll"
@@ -14,27 +15,37 @@ DEL "Phoenix\Binaries\Win64\openvr_api.dll"
 DEL "Phoenix\Binaries\Win64\RealConfig.bat"
 DEL "Phoenix\Binaries\Win64\RealVR.ini"
 DEL "Phoenix\Binaries\Win64\RealVR64.log"
-DEL "Phoenix\Binaries\Win64\RealConfig.bat"
 
 echo Restoring graphic settings...
 SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
 SET "dst=%LOCALAPPDATA%%sub%"
 DEL "%dst%GameUserSettings.ini"
-COPY "%dst%GameUserSettings_ori.ini" "%dst%GameUserSettings.ini"
-DEL "%dst%GameUserSettings_ori.ini"
+REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
 GOTO end
 
 
-:cleaningHogwarts2
+:cleanupHogwarts2
 echo Deleting mod files...
 RMDIR /s /q "RealRepo"
-DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log" "RealConfig.bat" 
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
 
 echo Restoring graphic settings...
 SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
 SET "dst=%LOCALAPPDATA%%sub%"
 DEL "%dst%GameUserSettings.ini"
-REN "%dst%GameUserSettings_ori.ini" "%dst%GameUserSettings.ini" 
+REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
+GOTO end
+
+:cleanupCP2077
+echo Deleting mod files...
+RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
+
+echo Restoring graphic settings...
+SET sub=\CD Projekt Red\Cyberpunk 2077\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%UserSettings.json"
+REN "%dst%UserSettings_ori.json" UserSettings.json
 GOTO end
 
 :end

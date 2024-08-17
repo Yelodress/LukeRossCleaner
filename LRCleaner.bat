239bdaed0a6e7204acb22134a1438640873c7446
Yelodress <82002356+Yelodress@users.noreply.github.com>
2024-08-17 15:43:58 +0200

    Adding Elden Ring support

--- LRCleaner.bat
+++ LRCleaner.bat
@@ -4,6 +4,7 @@ IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupH
 IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts2
 IF EXIST Cyberpunk2077.exe GOTO cleanupCP2077
 IF EXIST HorizonZeroDawn.exe GOTO cleanupHZD
+IF EXIST eldenring.exe IF EXIST start_protected_game_ori.exe GOTO cleanupEldenRing
 ECHO This game is not supported for now.
 GOTO :end
 
@@ -56,10 +57,22 @@ DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini"
 
 echo Restoring graphic settings...
 SET sub=\Horizon Zero Dawn\Saved Game\profile\
-
 SET "dst=%USERPROFILE%\Documents%sub%"
 DEL "%dst%graphicsconfig.ini"
 REN "%dst%graphicsconfig_ori.ini" graphicsconfig.ini
+GOTO end
+
+:cleanupEldenRing
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log" "start_protected_game.exe" "RealVR64.dll"
+REN start_protected_game_ori.exe start_protected_game.exe
+
+echo Restoring graphic settings...
+SET sub=\EldenRing\
+SET "dst=%APPDATA%%sub%"
+DEL "%dst%GraphicsConfig.xml"
+REN "%dst%GraphicsConfig_ori.xml" GraphicsConfig.xml
 GOTO end
 
 :end

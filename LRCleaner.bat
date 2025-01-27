b480b6da86f4a3bc11e6d7acfa35af0e4498b9dd
Yelodress <yelodress@pm.me>
2025-01-27 20:20:48 +0100

    Global refactor + added Ghostrunner support

--- LRCleaner.bat
+++ LRCleaner.bat
@@ -1,46 +1,18 @@
 @echo off
 
-IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts
-IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts2
 IF EXIST Cyberpunk2077.exe GOTO cleanupCP2077
-IF EXIST HorizonZeroDawn.exe GOTO cleanupHZD
-IF EXIST HorizonForbiddenWest.exe GOTO cleanupHFW
 IF EXIST eldenring.exe IF EXIST start_protected_game_ori.exe GOTO cleanupEldenRing
 IF EXIST FarCry4.exe GOTO cleanupFC4
 IF EXIST FarCry6.exe GOTO cleanupFC6
+IF EXIST Ghostrunner-Win64-Shipping.exe GOTO cleanupGR
+IF EXIST HorizonForbiddenWest.exe GOTO cleanupHFW
+IF EXIST HorizonZeroDawn.exe GOTO cleanupHZD
+IF EXIST HogwartsLegacy.exe IF NOT EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts
+IF EXIST HogwartsLegacy.exe IF EXIST EOSSDK-Win64-Shipping.dll GOTO cleanupHogwarts2
 IF EXIST Outlaws.exe GOTO cleanupStarWarsOutlaws
+IF EXIST Outlaws_Plus.exe GOTO cleanupStarWarsOutlaws
 ECHO This game is not supported for now.
 GOTO abort
-
-:cleanupHogwarts
-echo Deleting mod files...
-RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
-DEL "Phoenix\Binaries\Win64\cudart64_110.dll"
-DEL "Phoenix\Binaries\Win64\dxgi.dll"
-DEL "Phoenix\Binaries\Win64\openvr_api.dll"
-DEL "Phoenix\Binaries\Win64\RealConfig.bat"
-DEL "Phoenix\Binaries\Win64\RealVR.ini"
-DEL "Phoenix\Binaries\Win64\RealVR64.log"
-
-echo Restoring graphic settings...
-SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
-SET "dst=%LOCALAPPDATA%%sub%"
-DEL "%dst%GameUserSettings.ini"
-REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
-GOTO end
-
-
-:cleanupHogwarts2
-echo Deleting mod files...
-RMDIR /s /q "RealRepo"
-DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
-
-echo Restoring graphic settings...
-SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
-SET "dst=%LOCALAPPDATA%%sub%"
-DEL "%dst%GameUserSettings.ini"
-REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
-GOTO end
 
 :cleanupCP2077
 echo Deleting mod files...
@@ -52,27 +24,6 @@ SET sub=\CD Projekt Red\Cyberpunk 2077\
 SET "dst=%LOCALAPPDATA%%sub%"
 DEL "%dst%UserSettings.json"
 REN "%dst%UserSettings_ori.json" UserSettings.json
-GOTO end
-
-:cleanupHZD
-echo Deleting mod files...
-RMDIR /s /q "RealRepo"
-DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
-
-echo Restoring graphic settings...
-SET sub=\Horizon Zero Dawn\Saved Game\profile\
-SET "dst=%USERPROFILE%\Documents%sub%"
-DEL "%dst%graphicsconfig.ini"
-REN "%dst%graphicsconfig_ori.ini" graphicsconfig.ini
-GOTO end
-
-:cleanupHFW
-echo Deleting mod files...
-RMDIR /s /q "RealRepo"
-DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log" "RealVR64.dll"
-
-echo Restoring the graphics settings will delete the game graphics registry key. You will need to set your preferences again at the next launch.
-REG delete "HKCU\Software\Guerrilla Games\Horizon Forbidden West Complete Edition\Graphics"
 GOTO end
 
 :cleanupEldenRing
@@ -120,6 +71,67 @@ IF EXIST "%dst%\gamerprofile_ori.xml" (
 
 GOTO end
 
+:cleanupGR
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
+
+echo Restoring graphic settings...
+SET sub=\Ghostrunner\Saved\Config\WindowsNoEditor\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%GameUserSettings.ini"
+REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
+GOTO end
+
+:cleanupHFW
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log" "RealVR64.dll"
+
+echo Restoring the graphics settings will delete the game graphics registry key. You will need to set your preferences again at the next launch.
+REG delete "HKCU\Software\Guerrilla Games\Horizon Forbidden West Complete Edition\Graphics"
+GOTO end
+
+:cleanupHZD
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
+
+echo Restoring graphic settings...
+SET sub=\Horizon Zero Dawn\Saved Game\profile\
+SET "dst=%USERPROFILE%\Documents%sub%"
+DEL "%dst%graphicsconfig.ini"
+REN "%dst%graphicsconfig_ori.ini" graphicsconfig.ini
+GOTO end
+
+:cleanupHogwarts
+echo Deleting mod files...
+RMDIR /s /q "Phoenix\Binaries\Win64\RealRepo"
+DEL "Phoenix\Binaries\Win64\cudart64_110.dll"
+DEL "Phoenix\Binaries\Win64\dxgi.dll"
+DEL "Phoenix\Binaries\Win64\openvr_api.dll"
+DEL "Phoenix\Binaries\Win64\RealConfig.bat"
+DEL "Phoenix\Binaries\Win64\RealVR.ini"
+DEL "Phoenix\Binaries\Win64\RealVR64.log"
+
+echo Restoring graphic settings...
+SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%GameUserSettings.ini"
+REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
+GOTO end
+
+:cleanupHogwarts2
+echo Deleting mod files...
+RMDIR /s /q "RealRepo"
+DEL "cudart64_110.dll" "dxgi.dll" "openvr_api.dll" "RealConfig.bat" "RealVR.ini" "RealVR64.log"
+
+echo Restoring graphic settings...
+SET sub=\Hogwarts Legacy\Saved\Config\WindowsNoEditor\
+SET "dst=%LOCALAPPDATA%%sub%"
+DEL "%dst%GameUserSettings.ini"
+REN "%dst%GameUserSettings_ori.ini" GameUserSettings.ini
+GOTO end
 
 :cleanupStarWarsOutlaws
 echo Deleting mod files...
